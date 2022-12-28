ed920d8edb25fca055a3b2aa938c67c434a4314a
Manon Digne <manon.digne@gmail.com>
2022-12-28 15:28:01 +0100

    Intro - Commentaire et crea composant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
+// App.module est le module racine du projet
+// dans lequel on rajoutera les nvlles fonctions et modules
+
+// import crea-config de modules d'Angular
 import { NgModule } from '@angular/core';
+// module qui fournit des directives nécessaires à l'app
 import { BrowserModule } from '@angular/platform-browser';
 
+// import routeur
 import { AppRoutingModule } from './app-routing.module';
+// import premier composant
 import { AppComponent } from './app.component';
 
+// déclaration des modules
 @NgModule({
+  // composants et directives (non-modules)
   declarations: [
     AppComponent
   ],
+  // autres modules imbriqués dans ce module
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  // injection de dépendances
   providers: [],
+  // Premier composant au démarrage de l'app
   bootstrap: [AppComponent]
 })
 export class AppModule { }
