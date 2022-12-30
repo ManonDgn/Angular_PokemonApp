50ac704f5966091024d91023cf5ceefebe309570
Manon Digne <manon.digne@gmail.com>
2022-12-30 16:24:05 +0100

    input recherche dans le tableau + template pokemon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,30 @@
 // import de l'élément component nécessaire depuis Angular
 import { Component, OnInit } from '@angular/core';
 
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 // Détails du composant : selector = nom + template = html direct ou fichier à part
 @Component({
   selector: 'app-root',
-  template: `
-  <h1>Welcome to {{title}}!</h1>
-  <p>Hello {{pokemonsList[1]}}</p>
-  `
+  templateUrl: 'app.component.html'
 })
 // export du composant pour pouvoir le réutiliser
 export class AppComponent implements OnInit {
   title = 'Pokemon App';
-  pokemonsList = ['Bulbizarre', 'Salamèche', 'Carapuce']
+  pokemonsList: Pokemon[] = POKEMONS ;
+  pokemonSelected: Pokemon|undefined;
+  ngOnInit() {
+    console.table(this.pokemonsList);
+  }
 
-  ngOnInit(): void {
-    console.table(this.pokemonsList)
+  selectPokemon(pokemonId: string) {
+    const pokemon: Pokemon|undefined = this.pokemonsList.find(pokemon => pokemon.id == +pokemonId);
+    if(pokemon) {
+      console.log(`Vous avez demandé le pokemon ${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+    } else {
+      console.log(`Vous avez demandé un pokemon inexistant`);
+      this.pokemonSelected = pokemon;
+    }
   }
 }
